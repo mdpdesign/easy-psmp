9dddcdcb478db3ec7ed5256ed27a276b3ecd1f65
Marek <marek@mdpdesign.pl>
2024-02-09 07:52:31 +0000

    Add pytest tests

---
+++ tests/test_epsmp.py
@@ -0,0 +1,28 @@
+import pty
+import subprocess
+from subprocess import CompletedProcess
+
+import pytest
+
+
+@pytest.mark.parametrize("cmd", ["ssh", "scp"])
+def test_epsmp(cmd: str):
+
+    cmd_str: str = f"""
+        /usr/bin/env bash -c '
+            source .venv/bin/activate;
+            python epsmp.py {cmd} dummyhost
+        '
+    """
+
+    _, child_fd = pty.openpty()
+    process: CompletedProcess = subprocess.run(
+        cmd_str,
+        stdin=child_fd,
+        stdout=child_fd,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        shell=True,
+    )
+
+    assert process.returncode == 0
